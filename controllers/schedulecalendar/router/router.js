abe94ce8a1a7b8a0ae5e8e87f0770926e006bd06
imangolf1313 <osvaldo.lopez.is@unipolidgo.edu.mx>
2020-11-28 08:37:11 -0600

    router

--- controllers/schedulecalendar/router/router.js
+++ controllers/schedulecalendar/router/router.js
@@ -4,6 +4,11 @@ const controller = require('../controllers/agenta.controller');
 const router = Router();
 
 router.route('/eventos').get(controller.getAllEventsByDate);
-router.route('/evento').post(controller.createEvento)
+router.route('/evento').post(controller.createEvento);
+router.route('/eventoO').get(controller.getOneEvent);
+router.route('/eventoD').delete(controller.deleteEvent);
+router.route('eventoU').put(controller.updateEvent)
+
+
 
 module.exports = router;
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,6 @@
 module.exports = function(_req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     next();
 }
